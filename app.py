ccac85fdcc3f1f698a790bdb6c92d9dc311f74e1
they-call-me-ig <156603943+they-call-me-ig@users.noreply.github.com>
2024-02-12 14:17:22 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask,request
+from model import predict_model
+app=Flask(__name__)
+
+@app.route('/')
+def basic():
+    return 'Api server started...'
+
+@app.route('/soil',methods=['get'])
+def soil():
+    value=request.args.get('soil')
+    print(value)
+    response = predict_model(value)
+    return{'msg:':value}
+if __name__=="__main__":
+    app.run('0.0.0.0',port=5001)
