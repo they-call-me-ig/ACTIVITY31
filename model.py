328f75bb7e80e9f903e512023a89bf984c9ff8bb
they-call-me-ig <156603943+they-call-me-ig@users.noreply.github.com>
2024-02-12 15:26:37 +0530

    model.py

--- model.py
+++ model.py
@@ -1,9 +1,10 @@
-import pandas as pd
+ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 def predict_model(i):
     dataset=pd.read_csv('soil.csv')
+    dataset=dataset.dropna()
     x=dataset.iloc[:,1].values
     y=dataset.iloc[:,-1].values
     x=x.reshape(-1,1)
